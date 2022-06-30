59d9482e31126a630458a1a23709e288cf2b6453
AlbeTorres <albertocorreoficial@gmail.com>
2022-06-30 11:49:15 -0400

    mejorado header enlaces

--- src/components/home/UserOption/UserOption.js
+++ src/components/home/UserOption/UserOption.js
@@ -13,9 +13,9 @@ const UserOption = ()=>{
             <input type="checkbox" id="menu" className=" hidden nav-input"/>
                 {
                     logueado?<ul className='nav-menu nav-transicion'>
-                                <li className='p-2 w-full text-center nav-hover efecto'><Link to={"/"} className="w-full block md:text-2xl">Acceder</Link></li>
-                                <div className='h-px w-11/12 bg-white'></div>
-                                <li className='p-2 w-full text-center nav-hover efecto'><Link to={"/nueva-cuenta"} className="w-full block md:text-2xl">Regístrarse</Link></li>
+                                <li className='p-2 w-full text-center nav-hover efecto'><Link to={"/"} className="w-full block md:text-2xl enlace">Acceder</Link></li>
+                                <div className='h-px w-11/12 bg-white desktop'></div>
+                                <li className='p-2 w-full text-center nav-hover efecto'><Link to={"/nueva-cuenta"} className="w-full block md:text-2xl enlace">Regístrarse</Link></li>
                             </ul>
                             :<ul className='nav-menu nav-transicion'>
                                 <li className='p-2 w-full text-center nav-hover efecto md:text-2xl'>Perfil</li>
