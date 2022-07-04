a81faf5a249e5adf5a197afec3187deb069d3650
AlbeTorres <albertocorreoficial@gmail.com>
2022-07-04 11:36:53 -0400

    eso

--- src/components/home/UserOption/UserOption.js
+++ src/components/home/UserOption/UserOption.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{Fragment,useState} from 'react';
 import {Link} from 'react-router-dom';
 import './userOption.css'
 
@@ -6,24 +6,38 @@ const UserOption = ()=>{
 
     const logueado=true
     return(
+        <Fragment>
+
+        {logueado ?
+        <nav className='border-red-50 border-2'>
+        <label htmlFor="menu" className="cursor-pointer show ">
+                <img src="img/test.jpg" alt="" width="50px"  />
+            </label>
+            <input type="checkbox" id="menu" className=" hidden nav-input"/>
+
+            <ul className='nav-menu nav-transicion'>
+                <li className=''>Perfil</li>
+        
+                <li className=''>Cerrar sesión</li>
+            </ul>
+
+        </nav>:
         <nav >
             <label htmlFor="menu" className="cursor-pointer show ">
                 <img src='img/bx-menu.svg' alt="menu-icon" className='w-8 md:w-10'/>
             </label>
             <input type="checkbox" id="menu" className=" hidden nav-input"/>
-                {
-                    logueado?<ul className='nav-menu nav-transicion'>
-                                <li className='p-2 w-full text-center nav-hover efecto'><Link to={"/"} className="w-full block md:text-2xl enlace">Acceder</Link></li>
-                                <div className='h-px w-11/12 bg-white desktop'></div>
-                                <li className='p-2 w-full text-center nav-hover efecto'><Link to={"/nueva-cuenta"} className="w-full block md:text-2xl enlace">Regístrarse</Link></li>
-                            </ul>
-                            :<ul className='nav-menu nav-transicion'>
-                                <li className='p-2 w-full text-center nav-hover efecto md:text-2xl'>Perfil</li>
-                                <div className='h-px w-11/12 bg-white'></div>
-                                <li className='p-2 w-full text-center nav-hover efecto md:text-2xl'>Cerrar sesión</li>
-                            </ul>
-                } 
+                <ul className='nav-menu nav-transicion'>
+                    <li className='p-2 w-full text-center nav-hover efecto'><Link to={"/"} className="w-full block md:text-2xl enlace">Acceder</Link></li>
+                    <div className='h-px w-11/12 bg-white desktop'></div>
+                    <li className='p-2 w-full text-center nav-hover efecto'><Link to={"/nueva-cuenta"} className="w-full block md:text-2xl enlace">Regístrarse</Link></li>
+                </ul>
+                           
+                
         </nav>
+                } 
+
+        </Fragment>
     );
 }
 
