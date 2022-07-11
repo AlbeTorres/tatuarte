1f132b1ef25291104799a4f1a1391d4879dbb0de
albeTorres <albertocorreoficial@gmail.com>
2022-07-11 11:58:13 -0400

    editar estudio creado prototipo

--- src/pages/estudioForm/Estudioform.js
+++ src/pages/estudioForm/Estudioform.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './estudioForm.css';
-import {FaPlus} from 'react-icons/fa';
+import {FaPlus,FaRegCompass,FaRegMap, FaPinterestSquare, FaTwitterSquare,
+    FaWhatsappSquare, FaFacebookSquare,FaInstagramSquare, FaPaintBrush} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Estudioform = () => {
 
@@ -13,40 +15,55 @@ const Estudioform = () => {
 
 
   return (
-    <form onSubmit={onSubmit} className='flex flex-col items-center ' >
-        <label for='formImg' className=' rounded-full w-20 h-20 relative '>
-            <img  src='img/test.jpg' alt={''} className=' rounded-full w-full h-full object-cover'/>
-            <FaPlus className=' absolute bottom-1 right-2 text-white text-xl bg-gray-600 rounded-full p-1'></FaPlus>
-        </label>
-        <input id='formImg' type={'file'} alt={''} className='hidden h-full w-full'
-            accept='image/png, image/jpg '  /> 
-        <div>
-            <input type={ 'text '} placeholder={'Nombre Estudio'} />
+    <form onSubmit={onSubmit} className='flex flex-col items-center justify-center h-screen ' >
+        <div className=''>
+            <label htmlFor='formImg' className='rounded-full w-20 h-20 relative block '>
+                <img  src='img/test.jpg' alt={''} className=' rounded-full w-full h-full object-cover'/>
+                <FaPlus className=' absolute bottom-1 right-2 text-white text-xl bg-gray-600 rounded-full p-1'></FaPlus>
+            </label>
+            <input id='formImg' type={'file'} alt={''} className='hidden h-full w-full'
+                accept='image/png, image/jpg '  /> 
         </div>
-        <div>
-            <input type={ 'text'} placeholder={'Provincia'}/>
+
+
+        <div className=' input-estudioForm_div'>
+            <FaPaintBrush className='input-icon' ></FaPaintBrush>
+            <input className='input-estudioForm' type={ 'text '} placeholder={'Nombre Estudio'} />
         </div>
-        <div>
-            <input type={ 'text'}  placeholder={'Municipio'} />
+        
+        <div className=' input-estudioForm_div'>
+            <FaRegCompass className='input-icon'></FaRegCompass>
+            <input className='input-estudioForm' type={'text'} placeholder={'Provincia'}/>
         </div>
-        <div>
-            <input type={'url'} placeholder={'Pinteres'}/>
+        <div className=' input-estudioForm_div'>
+            <FaRegMap className='input-icon'></FaRegMap>
+            <input className='input-estudioForm' type={ 'text'}  placeholder={'Municipio'} />
         </div>
-        <div>
-            <input type={'url'} placeholder={'Twitter'}/>
+        <div className=' input-estudioForm_div'>
+            <FaPinterestSquare className='input-icon'></FaPinterestSquare>
+            <input className='input-estudioForm' type={'url'} placeholder={'Pinteres'}/>
         </div>
-        <div>
-            <input type={'url'} placeholder={'Instagram'}/>
+        <div className=' input-estudioForm_div'>
+            <FaTwitterSquare className='input-icon'></FaTwitterSquare>
+            <input className='input-estudioForm' type={'url'} placeholder={'Twitter'}/>
         </div>
-        <div>
-            <input type={'url'} placeholder={'Facebook'}/>
+        <div className=' input-estudioForm_div'>
+            <FaInstagramSquare className='input-icon'></FaInstagramSquare>
+            <input className='input-estudioForm' type={'url'} placeholder={'Instagram'}/>
         </div>
-        <div>
-            <input type={'url'} placeholder={'Whatsapp'}/>
+        <div className=' input-estudioForm_div'>
+            <FaFacebookSquare className='input-icon'></FaFacebookSquare>
+            <input className='input-estudioForm' type={'url'} placeholder={'Facebook'}/>
         </div>
-        <div>
-            <input type={'submit'} value='Editar' />
+        <div className=' input-estudioForm_div'>
+            <FaWhatsappSquare className='input-icon'></FaWhatsappSquare>
+            <input className='input-estudioForm' type={'url'} placeholder={'Whatsapp'}/>
         </div>
+        <div className='w-4/5 my-2'>
+            <input className='btn' type={'submit'} value='Aceptar' />
+            <Link to={'/estudio'} className='btn text-center my-2'>Cancelar</Link>
+        </div>
+        
 
 
     </form>
