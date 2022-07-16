6698230f402f31b2961992104182af5120e6e3cb
albeTorres <albertocorreoficial@gmail.com>
2022-07-16 13:11:47 -0400

    creo que terminado home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,12 +3,57 @@ import UserOption from '../../components/home/UserOption/UserOption'
 import {Link} from 'react-router-dom';
 import EstudioCard from '../../components/estudio/EstudioCard';
 import Busqueda from '../../components/home/Busqueda/Busqueda';
+import DropMenu from '../../components/dropDownMenu/DropDownMenu';
+import {FaCaretDown,FaRegCompass,FaRegMap} from 'react-icons/fa';
 import '../../index.css'
 
 const Home = ()=>{
 
     const[logeado, setLogeado]=useState(true);
     const[visible, setVisible]=useState(false);
+
+    const provinciaArray= [
+        {icon: <FaRegCompass/>,text:{type:'provincia', name:'Artemisa'} },
+        {icon: <FaRegCompass/>,text:{type:'provincia', name:'Mayabeque'}},
+        {icon: <FaRegCompass/>,text:{type:'provincia', name:'P. del Río'}},
+        {icon: <FaRegCompass/>,text:{type:'provincia', name:'La Habana'}},
+        {icon: <FaRegCompass/>,text:{type:'provincia', name:'Matanzas'}},
+        
+        
+    ];
+    const municipioArray= [
+        {icon:<FaRegMap/>,text:{type:'municipio', name:'Artemisa'}},
+        {icon:<FaRegMap/>,text:{type:'municipio', name:'Candelaria'}},
+        {icon:<FaRegMap/>,text:{type:'municipio', name:'B. Honda'}},
+        {icon:<FaRegMap/>,text:{type:'municipio', name:'Guanaja'}},
+        {icon:<FaRegMap/>,text:{type:'municipio', name:'Alquizar'}},
+        
+        
+    ];
+
+    const [filtro, setFiltro] = useState({
+        provincia:'provincia',
+        municipio: 'municipio'
+    });
+
+    const onSelect =(itemSelect)=>{
+
+        if(itemSelect.type !== 'municipio'){
+            setFiltro({
+                ...filtro,
+                provincia:itemSelect.name
+    
+            })
+        }else{
+            setFiltro({
+                ...filtro,
+                municipio:itemSelect.name
+    
+            })
+        }
+        
+
+    }
 
     const menuSetVisible=()=>{
         setVisible(!visible);
@@ -33,12 +78,13 @@ const Home = ()=>{
         </section>
 
         { visible  ?  
-          <div className="absolute grid grid-rows-2 justify-items-center items-center rounded-md text-4xl right-10 left-10 z-10 text-gray-600 contacto-link h-28 shadow-md">
-                            
-                            <Link to={'/home'} className='estudio-btn text-center'>Añadir</Link>
-                            <Link to={'/home'} className='estudio-btn text-center'>Editar</Link>
+          <div className="absolute  items-center rounded-md right-10 left-10 z-10 text-gray-600 contacto-link h-16 shadow-md md:w-80 md:mx-auto  ">
+            <div className='relative w-full flex items-center justify-between p-4 '>
+                <DropMenu icon={<FaCaretDown/>} name={filtro.provincia} arreglo={provinciaArray} funcion={onSelect} />
+                <DropMenu icon={<FaCaretDown/>} name={filtro.municipio} arreglo={municipioArray} funcion={onSelect}/>
+            </div>
 
-                        </div>   : null
+        </div>   : null
           } 
         <section className='grid gap-4 items-center  auto-rows-min    md:grid-cols-2 lg:grid-cols-3 md:gap-4  sectionHeigth  overflow-y-scroll p-1  w-11/12  md:w-4/5 mx-auto ' >
             <EstudioCard name={'La Tiza'} img={'img/test.jpg '} />
