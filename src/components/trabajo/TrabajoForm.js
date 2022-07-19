b3f43d208f96707a6a2216150741667f8b6bfb7d
AlbeTorres <albertocorreoficial@gmail.com>
2022-07-19 13:22:26 -0400

    añadir trabajo responsive

--- src/components/trabajo/TrabajoForm.js
+++ src/components/trabajo/TrabajoForm.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import{ Link} from 'react-router-dom';
+import{FaDollarSign,FaPlus} from 'react-icons/fa'
 
 const TrabajoForm = ({onClick}) => {
 
     
 
   return (
-        <form className='absolute top-10 left-1/2 -ml-36 w-72 h-72 z-20 bg-white rounded-md shadow-lg'>
-            <div>
-                <img src="" alt="" />
-            </div>
-            <div>
-                <input type="text" />
-            </div> 
+        <form className='absolute  top-32 left-1/2 -ml-36  w-72 md:-ml-48  md:w-96   z-20 bg-white rounded-md shadow-lg'>
+            <div className='mt-2 flex items-center justify-center flex-col w-4/5 mx-auto'>
+                <label htmlFor="formTrabImg" className='w-full mx-auto h-60 block relative cursor-pointer'>
+                    <img src="img/test.jpg" alt="" className='object-cover w-full h-full rounded-md ' />
+                    <FaPlus className=" absolute bottom-1 right-2 text-white text-4xl bg-gray-600 rounded-full p-1 shadow-md"></FaPlus>
+                </label>
+                <input id="formTrabImg" type={"file"} alt={""} className="hidden h-full w-full" accept="image/png, image/jpg "/>
+            
+        <div className=" my-2 flex items-center pl-1 w-full border-2 border-gray-500 rounded-md shadow-sm ">
+          <FaDollarSign className="input-icon"></FaDollarSign>
+          <input
+            className="input-estudioForm"
+            type={"text "}
+            placeholder={"Nombre Estudio"}
+          />
+        </div>
 
-            <div className="w-4/5 my-2">
+            <div className="w-full my-2">
                 <input className="btn" type={"submit"} value="Aceptar" />
                 <button to={"/estudio"} className="btn-black text-center my-2" onClick={()=>onClick()}>
                 Atrás
                 </button>
+        </div>
         </div>
         </form>
     
